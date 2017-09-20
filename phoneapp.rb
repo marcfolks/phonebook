1bb8a78062ac2462c0aa813db5c2c69463e3358c
marcfolks <marcfolks@gmail.com>
2017-09-20 16:04:07 -0400

    gitignore

--- phoneapp.rb
+++ phoneapp.rb
@@ -23,14 +23,14 @@ dbname: ENV['db_name'],
  
  
  post '/index' do 
-     fname = params[:user_given_first_name] 
-     lname = params[:user_given_last_name] 
-     street = params[:user_given_street_address] 
-     city = params[:user_given_city] 
-     state = params[:user_given_state] 
-     zip = params[:user_given_zip] 
-     phone = params[:user_given_phone_number] 
-     email = params[:user_given_email] 
+     fname = params[:fname] 
+     lname = params[:lname] 
+     street = params[:street] 
+     city = params[:city] 
+     state = params[:state] 
+     zip = params[:zipc] 
+     phone = params[:phone_number]
+   
 
 
 
@@ -38,4 +38,5 @@ dbname: ENV['db_name'],
 
 
 
-db.exec("INSERT INTO phonebook(first_name, last_name, street_address, city, state, zip, cell_phone, home_phone, work_phone) VALUES('#{first_name}', '#{last_name}', '#{street_address}', '#{city}', '#{state}', '#{zip}', '#{cell_phone}', '#{home_phone}', '#{work_phone}')");
+db.exec("INSERT INTO phonebook(first_name, last_name, street_address, city, state, zip, cell_phone, home_phone, work_phone) VALUES('#{fname}', '#{lname}', '#{street}', '#{city}', '#{state}', '#{zip}', '#{phone}')");
+ end
